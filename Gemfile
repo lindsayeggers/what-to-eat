0bfab649e69ad75b037dfc2496c8b0766382c706
Lindsay Eggers <lindsayeggers@bravowell.com>
2020-05-15 17:23:42 -0400

    initial
    oops forgot to commit stuff as I worked

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development, :test do
 end
 
 gem 'httparty', '0.18.0'
+gem 'figaro', '1.2.0'
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,16 +4,13 @@ class WelcomeController < ApplicationController
 	end
 
 	def search
-		response = HTTParty.get("https://api.foursquare.com/v2/search/recommendations?client_id=ZBFMRDI0GFSR5WP55VTUYLF5CXBADC3X4EN2OZTABMGQEHZJ&client_secret=24RBVH2L5LXADC1SEZCHXF3BOCSREGFAN2VVHDT3EFXQFBNX&near=#{params[:search]}&intent=food&v=20200501")
+		response = HTTParty.get("https://api.foursquare.com/v2/search/recommendations?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&near=#{params[:search]}&intent=food&v=20200501")
 		formattedReponse = JSON.parse(response.body)
 		totalNumOfResults = formattedReponse["response"]["group"]["totalResults"]
 		totalResults = formattedReponse["response"]["group"]["results"]
 		randomNum = rand(totalNumOfResults)
 		@eatHere = formattedReponse["response"]["group"]["results"][randomNum]
-		puts totalResults
 		puts "Eat here #{@eatHere}"
-		
-		
 		# puts randomNum
 		# totalResults = response['response']['totalResults']
 		# groups = response['response']['groups']
