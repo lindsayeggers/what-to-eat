95ff806bce575f657d086b0e9d2d528b666eac4b
lindsayeggers <lindsayeggers@bravowell.com>
2020-05-18 13:51:40 -0400

    fix how randomNum is calculated
    API returns up to 50 results per call. If the results array is less than the limit, use the lengh of results, otherwise use 49 (limit minus one).

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,25 +4,20 @@ class WelcomeController < ApplicationController
 	end
 
 	def search
-		response = HTTParty.get("https://api.foursquare.com/v2/search/recommendations?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&near=#{params[:search]}&intent=food&v=20200501")
+		response = HTTParty.get("https://api.foursquare.com/v2/search/recommendations?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&near=#{params[:search]}&intent=food&limit=50&v=20200501")
 		formattedReponse = JSON.parse(response.body)
-		totalNumOfResults = formattedReponse["response"]["group"]["totalResults"]
 		totalResults = formattedReponse["response"]["group"]["results"]
-		randomNum = rand(totalNumOfResults)
-		@eatHere = formattedReponse["response"]["group"]["results"][randomNum]
-		puts "Eat here #{@eatHere}"
-		# puts randomNum
-		# totalResults = response['response']['totalResults']
-		# groups = response['response']['groups']
-		# puts groups
-		# puts totalResults
+		totalNumOfResults = totalResults.length() - 1
 		
-		# randomNum = rand(@response['response']['venues'].length())
-		# @restaurant = @response['response']['venues'][randomNum]
-		# @address = @restaurant["location"]["address"]
-		# @city = @restaurant["location"]["city"]
-		# @state = @restaurant["location"]["state"]
-		# @zipCode = @restaurant["location"]["postalCode"]
+		if totalNumOfResults < 49
+			puts "less than 50"
+			randomNum = rand(totalNumOfResults)
+		else
+			puts "50 results"
+			randomNum = rand(49)
+		end
+		
+		@eatHere = formattedReponse["response"]["group"]["results"][randomNum]
 	end
 
 end
