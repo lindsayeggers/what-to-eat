f4da5c74bd09530f3665ffdc0dc680201194acb0
lindsayeggers <lindsayeggers@bravowell.com>
2020-05-19 17:09:37 -0400

    moved logic into model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,29 +5,11 @@ class WelcomeController < ApplicationController
 
 	def result
 		Restaurant.delete_all
-		response = HTTParty.get("https://api.foursquare.com/v2/search/recommendations?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&near=#{params[:location]}&intent=food&limit=50&v=20200501")
-		formattedReponse = JSON.parse(response.body)
-		totalResults = formattedReponse["response"]["group"]["results"]
-		totalResults.each do |result|
-			Restaurant.create(
-				name: result["venue"]["name"],
-				address: result["venue"]["location"]["address"],
-				city: result["venue"]["location"]["city"],
-				state: result["venue"]["location"]["state"],
-				zipCode: result["venue"]["location"]["postalCode"],
-				website: result["venue"]["url"]
-			)
-		end
-		restaurants = Restaurant.all
-		restaurants = restaurants.as_json
-		randomNum = rand(restaurants.length() - 1 )
-		@eatHere = restaurants[randomNum]
+		Restaurant.get_restaurants(params[:location])
+		@eatHere = Restaurant.get_random_restaurant()
 	end
 
 	def reload
-		restaurants = Restaurant.all
-		restaurants = restaurants.as_json
-		randomNum = rand(restaurants.length() - 1 )
-		@eatHere = restaurants[randomNum]
+		@eatHere = Restaurant.get_random_restaurant()
 	end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,26 @@
 class Restaurant < ApplicationRecord
+	def self.get_restaurants(location)
+		response = HTTParty.get("https://api.foursquare.com/v2/search/recommendations?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&near=#{location}&intent=food&limit=50&v=20200501")
+		formattedReponse = JSON.parse(response.body)
+		raw_results = formattedReponse["response"]["group"]["results"]
+		results = raw_results.map do |result|
+			{
+				name: result["venue"]["name"],
+				address: result["venue"]["location"]["address"],
+				city: result["venue"]["location"]["city"],
+				state: result["venue"]["location"]["state"],
+				zipCode: result["venue"]["location"]["postalCode"],
+				website: result["venue"]["url"]
+			}
+		end
+
+		Restaurant.import(results)
+	end
+
+	def self.get_random_restaurant
+		restaurants = Restaurant.all
+		randomNum = rand(restaurants.length() - 1)
+		restaurants[randomNum]
+	end
+	
 end
