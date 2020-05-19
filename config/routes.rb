2b29c43fb3d5cfb6eba76e831b690e9eb87dd305
lindsayeggers <lindsayeggers@bravowell.com>
2020-05-19 13:07:44 -0400

    Revert "change route to handle page refresh"
    This reverts commit 24421d29bb60088035278fdb48d20747c2c99170.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :welcome
   root 'welcome#index'
-	get '/result' => 'welcome#result'
+	post '/result' => 'welcome#result'
 	get '/reload' => 'welcome#reload'
 end
