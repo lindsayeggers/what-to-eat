4ead498e349276d4697704614d25f042f3d6b706
lindsayeggers <lindsayeggers@bravowell.com>
2020-06-17 13:16:48 -0400

    remove save to db, add some new restaurant info in

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,25 +2,26 @@ class Restaurant < ApplicationRecord
 	def self.get_restaurants(location)
 		offset = 0
 		limit = 50
-
+		results = []
 		loop do
 			response = HTTParty.get("https://api.foursquare.com/v2/search/recommendations?client_id=#{ENV['CLIENT_ID']}&client_secret=#{ENV['CLIENT_SECRET']}&near=#{location}&	intent=food&limit=#{limit}&offset=#{offset}&v=20200501")
 			formattedReponse = JSON.parse(response.body)
-			raw_results = formattedReponse["response"]["group"]["results"]
+			rawResults = formattedReponse["response"]["group"]["results"]
 			totalResults = formattedReponse["response"]["group"]["totalResults"]
-			results = raw_results.map do |result|
-				{
-					name: result["venue"]["name"],
-					address: result["venue"]["location"]["address"],
-					city: result["venue"]["location"]["city"],
-					state: result["venue"]["location"]["state"],
-					zipCode: result["venue"]["location"]["postalCode"],
-					website: result["venue"]["url"]
+			rawResults.each do |r|
+				results << {
+					name: r["venue"]["name"],
+					address: r["venue"]["location"]["address"],
+					city: r["venue"]["location"]["city"],
+					state: r["venue"]["location"]["state"],
+					zipCode: r["venue"]["location"]["postalCode"],
+					photo: r["photo"],
+					review: r["snippets"]["items"][0]["detail"]
 				}
 			end
-			Restaurant.import(results)
 			offset += limit
 			break if offset >= totalResults
 		end
+		results
 	end
 end
